7d070750e52c79737b30b287068156b8bdae3fbc
aditya007 <adityatandon007@gmail.com>
2019-07-30 20:52:52 +0530

    Updates

---
+++ precache-manifest.eb9358710981692ba4bb75d46a73b1ea.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d6d4f4ba5f040fed40f40513335757",
+    "url": "/react-redux-shopping-cart-app/index.html"
+  },
+  {
+    "revision": "933b605634e256607004",
+    "url": "/react-redux-shopping-cart-app/static/css/2.2c98c60f.chunk.css"
+  },
+  {
+    "revision": "24204ec31236879eaeea",
+    "url": "/react-redux-shopping-cart-app/static/css/main.ecc204ab.chunk.css"
+  },
+  {
+    "revision": "933b605634e256607004",
+    "url": "/react-redux-shopping-cart-app/static/js/2.f19874fb.chunk.js"
+  },
+  {
+    "revision": "24204ec31236879eaeea",
+    "url": "/react-redux-shopping-cart-app/static/js/main.a441e84d.chunk.js"
+  },
+  {
+    "revision": "77c2f66ec319173d9a4c",
+    "url": "/react-redux-shopping-cart-app/static/js/runtime~main.a7f1194d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-redux-shopping-cart-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-redux-shopping-cart-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-redux-shopping-cart-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-redux-shopping-cart-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-redux-shopping-cart-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
